834fb5a868d6dbf847a83327b1b1b61a3b1d45ed
kevinhuang.lds <kevin.huang@ischool.com.tw>
2013-08-06 12:15:44 +0800

    Add SignIn Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 WardHelper2::Application.routes.draw do
+  get "signin/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
