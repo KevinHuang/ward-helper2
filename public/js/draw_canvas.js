8eabe9bd901882d03d888a79f05fdf5ea3dfadec
kevinhuang.lds <kevin.huang@ischool.com.tw>
2013-08-07 19:45:10 +0800

    修正 jquery plugin 使之能調整 lineColor 及 lineWidth
    目前仍有問題：
    在 jquery plugin 中無法抓到後來設定的值，如 lineColor, lineWidth，這仍待解決。

--- public/js/draw_canvas.js
+++ public/js/draw_canvas.js
@@ -1,113 +1,126 @@
 
 	//定義 Touch drawing 行為的 jquery plugin
 	var defineTouchDrawing = function() {
-		$.fn.drawTouch = function(options) {
-			var drawCallback ;
-			var startCallback ;
-			var endCallback ;
-			var lineWidth = 4 ;
+		$.fn.drawTouch = function(option) {
 
-			if (options){
-				drawCallback = options.onDrawing ;
-				startCallback = options.onStart ;
-				endCallback = options.onEnd ;
 
-				if (options.lineWidth) {
-					lineWidth = options.lineWidth ;
-				}
-			}
-
-			var raiseDrawingCallback = function(x, y) {
-				if (drawCallback) {
-					drawCallback({time: (new Date()).getTime(), x: x, y: y, lw: lineWidth});
-				}
+			var defaults = {
+				onDrawing : null,
+				onStart : null,
+				onEnd : null,
+				lineWidth : 4,
+				lineColor : "#000000"
 			};
 
-			var el = document.getElementsByTagName("canvas")[0];
-		  	var ctx = el.getContext("2d");
-		  	ctx.lineWidth = lineWidth;
+			var _lineColor = (option.lineColor) ? (option.lineColor) : "#000000";
 
-		    var start = function(e) {
-		        var touchEvent = e.originalEvent.changedTouches[0];
-		        ctx.beginPath();  
-		        ctx.moveTo(touchEvent.pageX, touchEvent.pageY);
-		        
-		        if (startCallback){
-		        	startCallback();
-		        }
 
-		        raiseDrawingCallback(touchEvent.pageX, touchEvent.pageY);
-		    };
-		    var move = function(e) {
-		        var touchEvent = e.originalEvent.changedTouches[0];
-		        e.preventDefault();
-		        ctx.lineTo(touchEvent.pageX, touchEvent.pageY);
-		        ctx.stroke();
-		        raiseDrawingCallback(touchEvent.pageX, touchEvent.pageY);
-		    };
+			var options = $.extend(defaults, option) ;
 
-		    var end = function(e) {
-		    	if (endCallback)
-		    		endCallback();
-		    }
-		    $(this).touchstart(start);
-		    $(this).touchmove(move);
-		    $(this).touchend(end);
+			return this.each(function(){
+
+				var raiseDrawingCallback = function(x, y, color) {
+					if (options.onDrawing) {
+						options.onDrawing({time: (new Date()).getTime(), x: x, y: y }); //, lw: options.lineWidth, c: color});
+					}
+				};
+
+				var el = this;
+			  	var ctx = el.getContext("2d");
+			  	ctx.lineWidth = options.lineWidth;
+
+			    var start = function(e) {
+			        var touchEvent = e.originalEvent.changedTouches[0];
+			        ctx.beginPath();  
+			        ctx.moveTo(touchEvent.pageX, touchEvent.pageY);
+			        
+			        if (options.onStart){
+			        	options.onStart();
+			        }
+
+			        raiseDrawingCallback(touchEvent.pageX, touchEvent.pageY, _lineColor);
+			    };
+			    var move = function(e) {
+			        var touchEvent = e.originalEvent.changedTouches[0];
+			        e.preventDefault();
+			        ctx.lineTo(touchEvent.pageX, touchEvent.pageY);
+			        ctx.strokeStyle = options.lineColor ;
+			        ctx.stroke();
+			        raiseDrawingCallback(touchEvent.pageX, touchEvent.pageY, _lineColor);
+			        console.log(options);
+			    };
+
+			    var end = function(e) {
+			    	if (options.onEnd)
+			    		options.onEnd();
+			    }
+			    $(this).touchstart(start);
+			    $(this).touchmove(move);
+			    $(this).touchend(end);
+			});
 		};
 	};
 
+
 	//定義 mouse drawing 行為的 jquery plugin
 	var defineMouseDrawing = function() {
-		$.fn.drawMouse = function(options) {
-			var drawCallback ;
-			var startCallback ;
-			var endCallback ;
-			var lineWidth = 4 ;
+		$.fn.drawMouse = function(option) {
 
-			if (options){
-				drawCallback = options.onDrawing ;
-				startCallback = options.onStart ;
-				endCallback = options.onEnd ;
-				if (options.lineWidth) {
-					lineWidth = options.lineWidth ;
-				}
-			}
-
-			var raiseDrawingCallback = function(x, y) {
-				if (drawCallback){
-					drawCallback({time: (new Date()).getTime(), x: x, y: y, lw: lineWidth});
-				}
+			var defaults = {
+				onDrawing : null,
+				onStart : null,
+				onEnd : null,
+				lineWidth : 4,
+				lineColor : "#000000"
 			};
 
-			var el = document.getElementsByTagName("canvas")[0];
-		  	var ctx = el.getContext("2d");
-		  	ctx.lineWidth = lineWidth ;
-		    var clicked = 0;
-		    var start = function(e) {
-		        clicked = 1;
-		        ctx.beginPath();
-		        ctx.moveTo(e.pageX, e.pageY);
+			var options = $.extend(defaults, option) ;
 
-		        if (startCallback)
-		        	startCallback();
+			var _lineColor = (option.lineColor) ? (option.lineColor) : "#000000";
 
-		        raiseDrawingCallback(e.pageX, e.pageY);
-		    };
-		    var move = function(e) {
-		        if(clicked){
-		            ctx.lineTo(e.pageX, e.pageY);
-		            ctx.stroke();
-		            raiseDrawingCallback(e.pageX, e.pageY);
-		        }
-		    };
-		    var stop = function(e) {
-		        clicked = 0;
+			return this.each(function(){
 
-		        if (endCallback)
-		        	endCallback();
-		    };
-		    $(this).mousedown(start);
-		    $(this).mousemove(move);
-		    $(this).mouseup(stop);
+				var raiseDrawingCallback = function(x, y, color) {
+					if (options.onDrawing){
+						options.onDrawing({time: (new Date()).getTime(), x: x, y: y }); //, lw: options.lineWidth, c:color});
+					}
+				};
+
+				var el = this;
+			  	var ctx = el.getContext("2d");
+			  	ctx.lineWidth = options.lineWidth ;
+			    var clicked = 0;
+
+			    var start = function(e) {
+			        clicked = 1;
+			        ctx.beginPath();
+			        ctx.moveTo(e.pageX, e.pageY);
+
+			        if (options.onStart)
+			        	options.onStart();
+
+			        raiseDrawingCallback(e.pageX, e.pageY, options.lineColor);
+			    };
+			    var move = function(e) {
+			        if(clicked){
+			            ctx.lineTo(e.pageX, e.pageY);
+			            ctx.strokeStyle = options.lineColor ;
+			            //console.log("color:" + _lineColor); //為什麼設定幾次就會有幾個值？
+			            ctx.stroke();
+
+			            raiseDrawingCallback(e.pageX, e.pageY, _lineColor);
+			        }
+			    };
+			    var stop = function(e) {
+			        clicked = 0;
+
+			        if (options.onEnd)
+			        	options.onEnd();
+			    };
+			    $(this).mousedown(start);
+			    $(this).mousemove(move);
+			    $(this).mouseup(stop);
+
+			});
 		};
 	};
